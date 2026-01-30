ff2f1cde4372770a4a670e68d5900761022b7012
JonathanEJY <jonathanyont@gmail.com>
2026-01-30 18:01:40 -0300

    add: new pages and router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router";
+import router from "./routes";
 
-createRoot(document.getElementById('root')).render(
-  
-    <App />
-
-)
+createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router}></RouterProvider>,
+);
